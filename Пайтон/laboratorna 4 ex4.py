77d2e6c985d26c67f4f87940f0c0bfe5f9e74bc4
tod2002-profi <56667737+tod2002-profi@users.noreply.github.com>
2019-11-26 09:25:47 +0200

    Update laboratorna 4 ex4.py

--- Пайтон/laboratorna 4 ex4.py
+++ Пайтон/laboratorna 4 ex4.py
@@ -1,9 +1,9 @@
 x = int(input('Введіть x: '))
 if x<=0 and x<5:
     print('y = 1 ')
-if x<=5 and x<8:
+elif x<=5 and x<8:
     print('y = 2 ')
-if x<0 :
+elif x<0 :
     print('y = 3 ')
-if x>=8 :
+elif x>=8 :
     print('y = 4 ')
