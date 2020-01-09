149c3f20a2909dbc540afa4f7d45007c0e18fa26
tod2002-profi <56667737+tod2002-profi@users.noreply.github.com>
2020-01-09 20:14:00 +0200

    Update map2.py

--- Lab_map_filter_reduce/map2.py
+++ Lab_map_filter_reduce/map2.py
@@ -2,7 +2,7 @@
 #  Зясувати, чи є вектори перпендикулярними.
 vector1 = [float(input('Coordinate of vector 1: ')) for x in range(3)]
 vector2 = [float(input('Coordinate of vector 2: ')) for x in range(3)]
-coordinate_product = list(map(lambda x1, x2: x1 + x2, vector1, vector2))
+coordinate_product = list(map(lambda x1, x2: x1 * x2, vector1, vector2))
 if sum(coordinate_product) == 0:
     print('Perpendicular')
 else:
